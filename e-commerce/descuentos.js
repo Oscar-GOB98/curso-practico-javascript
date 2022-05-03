febbf5cf2e029f7740486f80eb092c1729ddd4ca
Oscar Ortiz <ogortizb@gmail.com>
2022-05-03 12:07:19 -0600

    taller 2 terminado

--- e-commerce/descuentos.js
+++ e-commerce/descuentos.js
@@ -12,3 +12,31 @@ function PriceDiscount(){
     const resultP = document.getElementById("ResultP");
     resultP.innerText = "Q"+PriceDiscountReal;
 }
+
+function Descuento1(){
+    alert('Se apliacara el descuento de 20%')
+    let Price = document.getElementById("InputPrice");
+    let InputPrice = Price.value;
+
+    porcentaje = 100 - 20;
+
+    let PriceDiscountReal = (InputPrice * porcentaje) / 100;
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "Q"+PriceDiscountReal;
+
+}
+
+function Descuento2(){
+    alert('Se apliacara el descuento de 30%')
+    let Price = document.getElementById("InputPrice");
+    let InputPrice = Price.value;
+
+    porcentaje = 100 - 30;
+
+    let PriceDiscountReal = (InputPrice * porcentaje) / 100;
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "Q"+PriceDiscountReal;
+
+}
