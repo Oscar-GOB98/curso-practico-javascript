5bfacedee7527406d7d0897734b256b28ba9da94
Oscar Ortiz <ogortizb@gmail.com>
2022-05-03 14:25:06 -0600

    clase 2 promedio

---
+++ estadistica/promedio.js
@@ -0,0 +1,17 @@
+function mAritmetica(lista){
+    let sumaLista = 0;
+    for (let i = 0; i< lista.length; i++){
+    sumaLista = sumaLista1 + lista[i];
+    }
+
+    /* const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento){
+            reuturn valorAcumulado + nuevoElemento;
+        }
+    );*/
+
+
+
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista;
+}
