2f719acc00e1927381d37d2ba44600f96f4c7c62
Oscar Ortiz <ogortizb@gmail.com>
2022-05-05 11:42:27 -0600

    termino el curso

---
+++ ProyectoFinal/calculos.js
@@ -0,0 +1,23 @@
+function ocultar1(){
+    document.getElementById('sirve').style.display = 'flex';
+    document.getElementById('sirve').style.justifyContent = 'center';
+    document.getElementById('extras').style.display = 'none';
+
+    
+}
+
+function ocultar2(){
+    document.getElementById('extras').style.display = 'flex';
+    document.getElementById('extras').style.justifyContent = 'center';
+    document.getElementById('sirve').style.display = 'none';
+}
+
+function ocultar3(){
+    document.getElementById('sirve').style.display = 'none';
+    document.getElementById('extras').style.display = 'none';
+}
+
+function ocultar4(){
+    document.getElementById('sirve').style.display = 'none';
+    document.getElementById('extras').style.display = 'none';
+}
