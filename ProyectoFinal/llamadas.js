63f444498cbb7159f2e3f3b425de669365c04469
Oscar Ortiz <ogortizb@gmail.com>
2022-05-05 16:41:21 -0600

    mi propia pagina

---
+++ ProyectoFinal/llamadas.js
@@ -0,0 +1,82 @@
+function ocultar(){
+    document.getElementById('home').style.display = 'flex';
+    document.getElementById('sirve').style.display = 'none';
+    document.getElementById('extras').style.display = 'none';
+    document.getElementById('sirve').style.display = 'none';
+    document.getElementById('extras').style.display = 'none';
+    document.getElementById('derecha').style.display = 'flex';
+    document.getElementById('derecha1').style.display = 'none';
+    document.getElementById('derecha2').style.display = 'none';
+    document.getElementById('derecha3').style.display = 'none';
+    document.getElementById('derecha4').style.display = 'none';
+    document.getElementById('derecha5').style.display = 'none';
+}
+
+function ocultar1(){
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('sirve').style.display = 'flex';
+    document.getElementById('sirve').style.justifyContent = 'center';
+    document.getElementById('extras').style.display = 'none';
+    document.getElementById('derecha').style.display = 'none';
+    document.getElementById('derecha1').style.display = 'flex';
+    document.getElementById('derecha2').style.display = 'none';
+    document.getElementById('derecha3').style.display = 'none';
+    document.getElementById('derecha4').style.display = 'none';
+    document.getElementById('derecha5').style.display = 'none';
+    document.getElementById('derecha5').style.display = 'none';
+
+    
+}
+
+function ocultar2(){
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('extras').style.display = 'flex';
+    document.getElementById('extras').style.justifyContent = 'center';
+    document.getElementById('sirve').style.display = 'none';
+    document.getElementById('derecha').style.display = 'none';
+    document.getElementById('derecha1').style.display = 'none';
+    document.getElementById('derecha2').style.display = 'flex';
+    document.getElementById('derecha3').style.display = 'none';
+    document.getElementById('derecha4').style.display = 'none';
+    document.getElementById('derecha5').style.display = 'none';
+}
+
+function ocultar3(){
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('sirve').style.display = 'none';
+    document.getElementById('extras').style.display = 'none';
+    document.getElementById('derecha').style.display = 'none';
+    document.getElementById('derecha1').style.display = 'none';
+    document.getElementById('derecha2').style.display = 'none';
+    document.getElementById('derecha3').style.display = 'flex';
+    document.getElementById('derecha4').style.display = 'none';
+    document.getElementById('derecha5').style.display = 'none';
+}
+
+function ocultar4(){
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('sirve').style.display = 'none';
+    document.getElementById('extras').style.display = 'none';
+    document.getElementById('sirve').style.display = 'none';
+    document.getElementById('extras').style.display = 'none';
+    document.getElementById('derecha').style.display = 'none';
+    document.getElementById('derecha1').style.display = 'none';
+    document.getElementById('derecha2').style.display = 'none';
+    document.getElementById('derecha3').style.display = 'none';
+    document.getElementById('derecha4').style.display = 'flex';
+    document.getElementById('derecha5').style.display = 'none';
+}
+
+function ocultar5(){
+    document.getElementById('home').style.display = 'flex';
+    document.getElementById('sirve').style.display = 'none';
+    document.getElementById('extras').style.display = 'none';
+    document.getElementById('sirve').style.display = 'none';
+    document.getElementById('extras').style.display = 'none';
+    document.getElementById('derecha').style.display = 'none';
+    document.getElementById('derecha1').style.display = 'none';
+    document.getElementById('derecha2').style.display = 'none';
+    document.getElementById('derecha3').style.display = 'none';
+    document.getElementById('derecha4').style.display = 'none';
+    document.getElementById('derecha5').style.display = 'flex';
+}
